f491d669addddc5b3b224a4406967f1c9f7a4bbe
Priyanshu79 <119873582+7Priyanshu9@users.noreply.github.com>
2023-09-13 14:53:40 +0530

    DAY1 uploaded

---
+++ DAY1.java
@@ -0,0 +1,33 @@
+// Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+ class Solution {
+    public static int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+
+    
+
+    for (int i = 1; i <= n; i++) {
+
+        // To calculate the number of 1's in binary representation of i,
+
+        // we can use the following recurrence relation:
+
+        // ans[i] = ans[i / 2] + (i % 2);
+
+        ans[i] = ans[i >> 1] + (i & 1);
+
+    }
+
+    
+
+    return ans;
+    }
+    public static void main(String[] args) {
+        int num = 7;
+        int ans[] = countBits(num);
+
+        for(int p : ans){
+            System.out.print(p);
+        }
+    }
+}
